8c51406f93033018dc3aee4019644300387c4e06
Jan Steikowski <j.steikowski@aol.com>
2023-07-02 17:25:55 +0700

    add font

--- src/generalElements.ts
+++ src/generalElements.ts
@@ -13,9 +13,10 @@ export function createHeader(): HTMLElement {
   // creating text in header
   const title: HTMLElement = document.createElement("p");
   title.innerText = "JanPomelo";
-  title.classList.add("text-3xl");
+  title.classList.add("text-3xl" ,'font-bold');
   const subTitle: HTMLElement = document.createElement("p");
   subTitle.innerText = "thoughts and other stuff.";
+  subTitle.classList.add('font-bold')
   header.appendChild(title);
   header.appendChild(subTitle);
   return header;
@@ -35,12 +36,13 @@ export function createFooter(): HTMLElement {
   );
   const footerText: HTMLElement = document.createElement("p");
   footerText.innerText = "All Rights Reserved";
+  footerText.classList.add('font-bold')
   footer.appendChild(footerText);
   return footer;
 }
 
 export function createMain(): HTMLElement {
   const main: HTMLElement = document.createElement("main");
-  main.classList.add("grow");
+  main.classList.add("grow", 'mx-5', 'md:mx-12' ,'xl:mx-20');
   return main;
 }
---
+++ src/welcomePage.ts
@@ -0,0 +1,8 @@
+
+const main: HTMLElement = document.querySelector('main');
+
+function createWelcome(): HTMLDivElement {
+  const div: HTMLDivElement = document.createElement('div');
+  
+  return div;
+}
