b3e9aa5272596b60348e894e5639f3f32b092614
Jan Steikowski <j.steikowski@aol.com>
2023-07-02 17:05:28 +0700

    Add footer content

--- src/generalElements.ts
+++ src/generalElements.ts
@@ -5,18 +5,42 @@ export function createHeader(): HTMLElement {
     "bg-black",
     "text-white",
     "flex",
-    'flex-col',
+    "flex-col",
     "justify-center",
-    "items-center"
+    "items-center",
+    "h-16"
   );
   // creating text in header
   const title: HTMLElement = document.createElement("p");
   title.innerText = "JanPomelo";
-  title.classList.add('text-3xl');
-  const subTitle: HTMLElement = document.createElement('p');
-  subTitle.innerText = 'thoughts and other stuff.'
+  title.classList.add("text-3xl");
+  const subTitle: HTMLElement = document.createElement("p");
+  subTitle.innerText = "thoughts and other stuff.";
   header.appendChild(title);
   header.appendChild(subTitle);
-  
   return header;
+}
+
+export function createFooter(): HTMLElement {
+  // createing footer element
+  const footer: HTMLElement = document.createElement("footer");
+  footer.classList.add(
+    "bg-black",
+    "text-white",
+    "flex",
+    "flex-col",
+    "justify-center",
+    "items-center",
+    "h-16"
+  );
+  const footerText: HTMLElement = document.createElement("p");
+  footerText.innerText = "All Rights Reserved";
+  footer.appendChild(footerText);
+  return footer;
+}
+
+export function createMain(): HTMLElement {
+  const main: HTMLElement = document.createElement("main");
+  main.classList.add("grow");
+  return main;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { createHeader } from "./generalElements";
+import { createHeader, createFooter, createMain } from "./generalElements";
 import "./style.css";
 
 const body = document.querySelector("body");
 body.appendChild(createHeader());
+body.appendChild(createMain());
+body.appendChild(createFooter());
